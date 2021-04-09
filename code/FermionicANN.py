8a1829eedcc39c5a8c8a8f810979d2aa405a392e
Yannick Stulens <yannick.stulens@ugent.be>
2021-04-09 21:34:49 +0200

    added argparse for job_array indexing

--- code/FermionicANN.py
+++ code/FermionicANN.py
@@ -6,6 +6,7 @@ import json
 
 import utility as ut
 import TrainingRBM as rbm
+import system_dicts
 # import TrainingRNN as rnn
 
 
@@ -14,14 +15,13 @@ def run_FRBM(systemData={}, alpha=2, learningrate=0.01, optimizer='sgd', numsamp
     # Call function from TrainingRBM.py
     rbm.run_RBM(systemData=systemData, outfile='', alpha=alpha, lr=learningrate, opt=optimizer, samples=numsamples, steps=numsteps, seed=seed)
 
-
     return 0
 
 
 def run_FRNN():
 
     # Call function from TrainingRBM.py
-
+    rnn.run_RNN(systemData=systemData, N = 10, num_units = 50, num_layers = 1, learningrate = 5e-3, lrschedule='C', numsamples = 100000, numsteps = 2000, seed = 123)
 
     return 0
 
@@ -29,10 +29,18 @@ def run_FRNN():
 
 
 
-
-
 if __name__ == '__main__':
-    # systemData = ut.make_dict(atom='H2', basis='STO-3G')
-    run_FRBM()
+    import argparse
+    parser = argparse.ArgumentParser(description='Run optimization for 1 systemData')
+    parser.add_argument('-index', type=int)
+    parser.add_argument('-machine', type=str)
+    args = parser.parse_args()
 
-    # integralData = 
+    # example for dissociation curve H2
+    if args.index: system = system_dicts.sto3g_H2[args.index]
+    else: system = system_dicts.sto3g_H2[3] #eq config
+
+    if args.machine == 'rbm':
+        run_FRBM(systemData=system)
+    if args.machine == 'rnn':
+        run_FRBM(systemData=system)
