4bbba6c2bff5f9c6949c39eb81ef4358311e9f77
Siebe Frederix <siebe.frederix@ugent.be>
2021-04-12 17:50:16 +0200

    ready for the LiH gridsearch

--- code/FermionicANN.py
+++ code/FermionicANN.py
@@ -37,17 +37,18 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
 
-    save_dir = 'dissociation_H2'
+#     save_dir = 'dissociation_LiH'
+    save_dir = 'sweep_LiH'
     
     #dissociation curve H2
-    system = sto3g_H2[args.index]
+#     system = sto3g_LiH[args.index]
     
-    '''
+    
     #single eq run
-    system = sto3g_H2_eq
-    '''
+    system = sto3g_LiH_eq
+    
 
-    '''
+    
     #RBM - grid
     if args.machine == 'rbm':
         grid = dict(
@@ -58,7 +59,8 @@ if __name__ == '__main__':
         )
         combos = [i for i in itertools.product(*list(grid.values()))]
         alpha, lr, opt, trick = combos[args.index]
-
+    
+    '''
     # RNN - grid
     if args.machine == 'rnn':
         grid = dict(
@@ -73,8 +75,8 @@ if __name__ == '__main__':
 
 
     if args.machine == 'rbm':
-        run_FRBM(systemData=system, alpha=1, lr=0.1, opt='sgd', numsamples=10000, use_sampler_init_trick=True, numsteps=3000, save_dir=save_dir) # use this for a single run
-        #run_FRBM(systemData=system, alpha=alpha, lr=lr, opt=opt, numsamples=1000, use_sampler_init_trick=trick, numsteps=500, save_dir=save_dir) # or this for argumetns from grid
+#         run_FRBM(systemData=system, alpha=1, lr=0.1, opt='sgd', numsamples=10000, use_sampler_init_trick=True, numsteps=3000, save_dir=save_dir) # use this for a single run
+        run_FRBM(systemData=system, alpha=alpha, lr=lr, opt=opt, numsamples=10000, use_sampler_init_trick=trick, numsteps=3000, save_dir=save_dir) # or this for argumetns from grid
     if args.machine == 'rnn':
         #run_FRNN(systemData=system, num_units = 50, num_layers = 1, learningrate = 5e-3, lrschedule='C', numsamples = 100000, numsteps = 1000, save_dir=save_dir)                     # use this for a single run
         run_FRNN(systemData=system, num_units = num_units, num_layers = num_layers, learningrate = lr, lrschedule=lrschedule, numsamples = 1000, numsteps = 2, save_dir=save_dir) # or this for argumetns from grid
