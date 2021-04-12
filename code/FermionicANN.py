ba0e4f6abb76fa841511c55f7568d1048d5db52b
Yannick Stulens <yannick.stulens@ugent.be>
2021-04-12 17:01:48 +0200

    disso params

--- code/FermionicANN.py
+++ code/FermionicANN.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
 
-    save_dir = 'dis_curve_H2'
+    save_dir = 'dissociation_H2'
     
     #dissociation curve H2
     system = sto3g_H2[args.index]
@@ -73,7 +73,7 @@ if __name__ == '__main__':
 
 
     if args.machine == 'rbm':
-        run_FRBM(systemData=system, alpha=1, lr=0.1, opt='sgd', numsamples=1000, use_sampler_init_trick=True, numsteps=2000, save_dir=save_dir) # use this for a single run
+        run_FRBM(systemData=system, alpha=1, lr=0.1, opt='sgd', numsamples=10000, use_sampler_init_trick=True, numsteps=3000, save_dir=save_dir) # use this for a single run
         #run_FRBM(systemData=system, alpha=alpha, lr=lr, opt=opt, numsamples=1000, use_sampler_init_trick=trick, numsteps=500, save_dir=save_dir) # or this for argumetns from grid
     if args.machine == 'rnn':
         #run_FRNN(systemData=system, num_units = 50, num_layers = 1, learningrate = 5e-3, lrschedule='C', numsamples = 100000, numsteps = 1000, save_dir=save_dir)                     # use this for a single run
