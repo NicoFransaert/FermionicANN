2ea85ee871df641e47dd4a987e710db15509fc08
Yannick Stulens <yannick.stulens@ugent.be>
2021-04-12 22:20:10 +0200

    double run

--- code/FermionicANN.py
+++ code/FermionicANN.py
@@ -32,51 +32,41 @@ def run_FRNN(systemData, num_units = 50, num_layers = 1, learningrate = 5e-3, lr
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='Run optimization for 1 system')
-    parser.add_argument('-index', default=0, type=int)
+    parser.add_argument('-index', default=53, type=int)
     parser.add_argument('-machine', default='rbm', type=str)
     args = parser.parse_args()
 
-
-#     save_dir = 'dissociation_LiH'
-    save_dir = 'sweep_LiH'
-    
-    #dissociation curve H2
-#     system = sto3g_LiH[args.index]
-    
-    
-    #single eq run
-    system = sto3g_LiH_eq
-    
-
-    
     #RBM - grid
     if args.machine == 'rbm':
+        system = sto3g_LiH_eq
+        save_dir = 'sweep2_LiH'
         grid = dict(
             alpha = [1, 2, 4],
             lr = [0.1, 0.01, 0.001],
             opt = ['sgd', 'adamax'],
-            trick = [False, True],
+            numsamples = [1000, 10000, 100000],
         )
         combos = [i for i in itertools.product(*list(grid.values()))]
-        alpha, lr, opt, trick = combos[args.index]
+        alpha, lr, opt, numsamples = combos[args.index]
     
-    '''
     # RNN - grid
     if args.machine == 'rnn':
+        system = sto3g_H2_eq
+        save_dir = 'sweep_H2'
         grid = dict(
-            num_units = [50],
-            num_layers = [1],
-            lr = [5e-3],
-            lrschedule = ['C']
+            num_units = [50, 100],
+            num_layers = [1, 2],
+            lr = [5e-3, 1e-3, 2.5e-4],
+            lrschedule = ['C', 'O'],
+            numsamples = [1000, 10000],
         )
-        combos = [i for i in itertools.product(*list(grid.values()))]
-        num_units, num_layers, lr, lrschedule, Complex = combos[args.index]
-    '''
+        combos = [i for i in itertools.product(*list(grid.values()))] + [50, 1, 5e-3, 'O', 100000]
+        num_units, num_layers, lr, lrschedule, numsamples = combos[args.index]
 
 
     if args.machine == 'rbm':
-#         run_FRBM(systemData=system, alpha=1, lr=0.1, opt='sgd', numsamples=10000, use_sampler_init_trick=True, numsteps=3000, save_dir=save_dir) # use this for a single run
-        run_FRBM(systemData=system, alpha=alpha, lr=lr, opt=opt, numsamples=10000, use_sampler_init_trick=trick, numsteps=3000, save_dir=save_dir) # or this for argumetns from grid
+        #run_FRBM(systemData=system, alpha=1, lr=0.1, opt='sgd', numsamples=10000, use_sampler_init_trick=True, numsteps=3000, save_dir=save_dir) # use this for a single run
+        run_FRBM(systemData=system, alpha=alpha, lr=lr, opt=opt, numsamples=numsamples, use_sampler_init_trick=True, numsteps=4000, save_dir=save_dir) # or this for argumetns from grid
     if args.machine == 'rnn':
-        #run_FRNN(systemData=system, num_units = 50, num_layers = 1, learningrate = 5e-3, lrschedule='C', numsamples = 100000, numsteps = 1000, save_dir=save_dir)                     # use this for a single run
-        run_FRNN(systemData=system, num_units = num_units, num_layers = num_layers, learningrate = lr, lrschedule=lrschedule, numsamples = 1000, numsteps = 2, save_dir=save_dir) # or this for argumetns from grid
+        #run_FRNN(systemData=system, num_units = 50, num_layers = 1, learningrate = 5e-3, lrschedule='C', numsamples = 100000, numsteps = 1000, save_dir=save_dir)                          # use this for a single run
+        run_FRNN(systemData=system, num_units = num_units, num_layers = num_layers, learningrate = lr, lrschedule=lrschedule, numsamples = numsamples, numsteps = 5000, save_dir=save_dir) # or this for argumetns from grid
