8061457ebaf287fcb174b9d0a89dee869d97332b
Yannick Stulens <yannick.stulens@ugent.be>
2021-04-13 12:58:41 +0200

    move to map

--- code/FermionicANN.py
+++ code/FermionicANN.py
@@ -32,16 +32,16 @@ def run_FRNN(systemData, num_units = 50, num_layers = 1, learningrate = 5e-3, lr
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='Run optimization for 1 system')
-    parser.add_argument('-index', default=53, type=int)
+    parser.add_argument('-index', default=0, type=int)
     parser.add_argument('-machine', default='rbm', type=str)
     args = parser.parse_args()
 
     #RBM - grid
     if args.machine == 'rbm':
-        system = sto3g_LiH_eq
+        system = sto3g_H2_eq
         save_dir = 'sweep2_LiH'
         grid = dict(
-            alpha = [1, 2, 4],
+            alpha = [1, 2],
             lr = [0.1, 0.01, 0.001],
             opt = ['sgd', 'adamax'],
             numsamples = [1000, 10000, 100000],
@@ -60,13 +60,14 @@ if __name__ == '__main__':
             lrschedule = ['C', 'O'],
             numsamples = [1000, 10000],
         )
-        combos = [i for i in itertools.product(*list(grid.values()))] + [50, 1, 5e-3, 'O', 100000]
+        combos = [i for i in itertools.product(*list(grid.values()))] + [(50, 1, 5e-3, 'O', 100000)]
         num_units, num_layers, lr, lrschedule, numsamples = combos[args.index]
 
+    save_dir='H2_for_plot'
 
     if args.machine == 'rbm':
-        #run_FRBM(systemData=system, alpha=1, lr=0.1, opt='sgd', numsamples=10000, use_sampler_init_trick=True, numsteps=3000, save_dir=save_dir) # use this for a single run
-        run_FRBM(systemData=system, alpha=alpha, lr=lr, opt=opt, numsamples=numsamples, use_sampler_init_trick=True, numsteps=4000, save_dir=save_dir) # or this for argumetns from grid
+        run_FRBM(systemData=system, alpha=1, lr=0.01, opt='sgd', numsamples=10000, use_sampler_init_trick=True, numsteps=6000, save_dir=save_dir) # use this for a single run
+        #run_FRBM(systemData=system, alpha=alpha, lr=lr, opt=opt, numsamples=numsamples, use_sampler_init_trick=True, numsteps=4000, save_dir=save_dir) # or this for argumetns from grid
     if args.machine == 'rnn':
         #run_FRNN(systemData=system, num_units = 50, num_layers = 1, learningrate = 5e-3, lrschedule='C', numsamples = 100000, numsteps = 1000, save_dir=save_dir)                          # use this for a single run
         run_FRNN(systemData=system, num_units = num_units, num_layers = num_layers, learningrate = lr, lrschedule=lrschedule, numsamples = numsamples, numsteps = 5000, save_dir=save_dir) # or this for argumetns from grid
--- code/plot.py
+++ code/plot.py
@@ -5,8 +5,9 @@ import json
 meta_file = '../data/RBM_runs/rbm_sto3g_H2_0-7348_eq1_a1_sgd_lr1_ns10000.META'
 hist_file = '../data/RBM_runs/rbm_sto3g_H2_0-7348_eq1_a1_sgd_lr1_ns10000.log'
 
-meta_file = '../data/RBM_runs/rbm_sto3g_H2_0-7348_eq1_a1_sgd_lr1_ns10000_trick.META'
-hist_file = '../data/RBM_runs/rbm_sto3g_H2_0-7348_eq1_a1_sgd_lr1_ns10000_trick.log'
+meta_file = '../data/RBM_runs/H2_for_plot/rbm_sto3g_H2_0-7348_eq1_a1_sgd_lr01_ns10000_trick.META'
+hist_file = '../data/RBM_runs/H2_for_plot/rbm_sto3g_H2_0-7348_eq1_a1_sgd_lr01_ns10000_trick.log'
+
 with open(meta_file) as jf:
     meta = json.load(jf)
 with open(hist_file) as jf:
@@ -15,19 +16,21 @@ it = [i['Iteration'] for i in hist['Output']]
 en = [i['Energy']['Mean'] + meta['SystemData']['nuc_rep_energy'] for i in hist['Output']]
 exact = meta['SystemData']['tot_energy']
 
+from scipy.signal import savgol_filter
+
 #plot energy history
-plt.plot(it, en)
+plt.plot(np.array(it), savgol_filter(en,51,3))
 plt.title(r'$H_2$ @ equilibrium')
 plt.xlabel('Iteration')
 plt.ylabel('Energy')
-plt.savefig('../data/plots/{}_E_hist.png'.format('H2eq'))
+plt.savefig('../data/plots/{}_E_hist_plot.png'.format('H2eq'))
 plt.clf()
 
 #plot relative energy error
-plt.plot(it, np.abs((np.array(en)-exact)/exact))
+plt.plot(np.array(it), savgol_filter(np.abs((np.array(en)-exact)/exact),51,3))
 plt.title(r'$H_2$ @ equilibrium')
 plt.xlabel('Iteration')
 plt.ylabel(r'$\Delta E_0$')
 plt.yscale('log')
-plt.savefig('../data/plots/{}_dE_hist.png'.format('H2eq'))
+plt.savefig('../data/plots/{}_dE_hist_plot.png'.format('H2eq'))
 plt.clf()
